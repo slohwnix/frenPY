281e7665346d6282829cd80ac35ad5fffa412489
slohwnix <115403305+slohwnix@users.noreply.github.com>
2024-12-26 14:52:29 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='frenpy',
+    packages=find_packages(include=['frenpy']),
+    version='0.1.0',
+    description='French python',
+    author='slohwnix',
+    install_requires=[]
+)
