eb58cb142573b31333758b4a110261d9e3ba5ddf
slohwnix <115403305+slohwnix@users.noreply.github.com>
2024-12-28 11:11:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='frenpy',
     packages=find_packages(include=['frenpy']),
-    version='0.3.4',
+    version='0.3.5',
     description='French python',
     author='slohwnix',
     install_requires=[],
